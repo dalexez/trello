169e65c774d9ece242267a6756b6ac3b60c2cdaf
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-05-04 00:08:03 -0500

    Agrendo forma de hacer request de forma local

---
+++ app1.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const  Trello = require('./node_modules/trello/main');
+
+var trello = new Trello(process.env.KEY, process.env.TOKEN);
+var cardTitle = `Card Nueva desde Node.js`;
+
+console.log("Ejecutando!");
+console.log(process.env.KEY);
+
+trello.addCard(cardTitle, "LaunchX Card Description", "627038f5421c536a1febcc75",
+  function (error, trelloCard) {
+    
+    console.log(error)
+    console.log(trelloCard)
+
+
+    
+    if (error) {
+      console.log('Could not add card:', error);
+    }
+    else {
+      console.log('Added card:', trelloCard);
+    }
+  });
